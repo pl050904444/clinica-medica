b3f88b8f16e911eb086102ad6ce59d28f8b620a5
evertonhf <evertonhf@gmail.com>
2024-05-22 19:49:17 -0300

    criando a estrutura do projeto

---
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/EmployerController.java
@@ -0,0 +1,11 @@
+package br.edu.imepac.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "employers")
+public class EmployerController {
+
+    
+}
